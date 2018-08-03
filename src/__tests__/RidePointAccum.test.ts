85fc9d142f2b8e455da1220272a925b05b6a4178
Jim Englert <englert.james@gmail.com>
2018-08-03 06:49:15 -0400

    Adding typescript + jest support

---
+++ src/__tests__/RidePointAccum.test.ts
@@ -0,0 +1,6 @@
+describe('Ride Point Accum', () => {
+    it('should add two number', () => {
+        const result = 1 + 1;
+        expect(result).toEqual(2);
+    });
+});
--- webpack.config.server.js
+++ webpack.config.server.js
@@ -19,7 +19,15 @@ module.exports = {
             test: /\.js?$/,
             use: 'babel-loader',
             exclude: /node_modules/
+        },
+        {
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
         }]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
     },
     plugins: [
         new StartServerPlugin({
